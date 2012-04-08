c28a90b0e67acd92d3768ac7dba402f389aa8155
Filip Hajny <filip@joyent.com>
2012-04-08 18:55:33 +0000

    Proper patch for the database cleanup code, from <imil>

--- pkgtools/pkgin/patches/patch-summary.c
+++ pkgtools/pkgin/patches/patch-summary.c
@@ -2,6 +2,24 @@ $NetBSD$
 
 --- summary.c.orig	2011-10-29 14:06:22.000000000 +0000
 +++ summary.c
+@@ -46,7 +46,7 @@ static const struct Summary {
+ 	const char	*provides;
+ 	const char	*end;
+ } sumsw[] = {
+-	{
++	[LOCAL_SUMMARY] = {
+ 		LOCAL_SUMMARY,
+ 		"LOCAL_PKG",
+ 		"LOCAL_DEPS",
+@@ -55,7 +55,7 @@ static const struct Summary {
+ 		"LOCAL_PROVIDES",
+ 		NULL
+ 	},
+-	{
++	[REMOTE_SUMMARY] = {
+ 		REMOTE_SUMMARY,
+ 		"REMOTE_PKG",
+ 		"REMOTE_DEPS",
 @@ -337,7 +337,7 @@ child_table(const char *fmt, ...)
  static void
  update_col(struct Summary sum, int pkgid, char *line)
@@ -11,50 +29,43 @@ $NetBSD$
  	int				i;
  	char			*val, *p, buf[BUFSIZ];
  
-@@ -536,6 +536,22 @@ delete_remote_tbl(struct Summary sum, ch
- 		"DELETE FROM REMOTE_PKG WHERE REPOSITORY = '%s';", repo);
- 	pkgindb_doquery(buf, NULL, NULL);
- }
-+static void
-+delete_remote_tbl_each(char *ptbl, char *repo)
-+{
-+	char buf[BUFSIZ];
-+
-+	snprintf(buf, BUFSIZ, DELETE_REMOTE,
-+		ptbl, ptbl, ptbl, ptbl, ptbl, repo, ptbl);
-+	pkgindb_doquery(buf, NULL, NULL);
-+
-+	if (strstr(ptbl, "_DEPS") != NULL)
-+	{
-+		snprintf(buf, BUFSIZ,
-+			"DELETE FROM REMOTE_PKG WHERE REPOSITORY = '%s';", repo);
-+		pkgindb_doquery(buf, NULL, NULL);
-+	}
-+}
+@@ -511,24 +511,18 @@ insert_summary(struct Summary sum, char 
+ static void
+ delete_remote_tbl(struct Summary sum, char *repo)
+ {
+-	char	*ptbl, buf[BUFSIZ];
+-	int		i, nelms;
++	char		buf[BUFSIZ];
++	const char	**arr;
  
- static int
- pdb_clean_remote(void *param, int argc, char **argv, char **colname)
-@@ -556,7 +572,10 @@ pdb_clean_remote(void *param, int argc, 
+-	/* number of elements in sum */
+-	nelms = (sizeof(sum) - sizeof(int)) / sizeof(char *) - 1;
+ 	/*
+ 	 * delete repository related tables
+ 	 * loop through sumsw structure to record table name
+ 	 * and call associated SQL query
+ 	 */
+-	for (ptbl = __UNCONST(sum.tbl_name), i = 0;
+-		 i < nelms;
+-		 ptbl += ((strlen(ptbl) + 1) * sizeof(char)), i++) {
+-
+-		if (strstr(ptbl, "_PKG") != NULL)
+-			continue;
+-
+-		snprintf(buf, BUFSIZ, DELETE_REMOTE, ptbl, ptbl, repo, ptbl);
++	/* (REMOTE[LOCAL)_PKG is first -> skip */
++	for (arr = &(sum.tbl_name) + 1; *arr != NULL; ++arr) {
++		snprintf(buf, BUFSIZ, DELETE_REMOTE,
++			*arr, *arr, *arr, *arr, *arr, repo, *arr);
+ 		pkgindb_doquery(buf, NULL, NULL);
+ 	}
+ 
+@@ -556,7 +550,7 @@ pdb_clean_remote(void *param, int argc, 
  	/* did not find argv[0] (db repository) in pkg_repos */
  	printf(MSG_CLEANING_DB_FROM_REPO, argv[0]);
  
 -	delete_remote_tbl(sumsw[1], argv[0]);
-+	delete_remote_tbl_each("REMOTE_DEPS", argv[0]);
-+	delete_remote_tbl_each("REMOTE_CONFLICTS", argv[0]);
-+	delete_remote_tbl_each("REMOTE_REQUIRES", argv[0]);
-+	delete_remote_tbl_each("REMOTE_PROVIDES", argv[0]);
++	delete_remote_tbl(sumsw[REMOTE_SUMMARY], argv[0]);
  
  	snprintf(query, BUFSIZ,
  		"DELETE FROM REPOS WHERE REPO_URL = \'%s\';", argv[0]);
-@@ -668,7 +687,10 @@ update_db(int which, char **pkgkeep)
- 				printf(MSG_PROCESSING_REMOTE_SUMMARY, *prepos);
- 
- 				/* delete remote* associated to this repository */
--				delete_remote_tbl(sumsw[i], *prepos);
-+				delete_remote_tbl_each("REMOTE_DEPS", *prepos);
-+				delete_remote_tbl_each("REMOTE_CONFLICTS", *prepos);
-+				delete_remote_tbl_each("REMOTE_REQUIRES", *prepos);
-+				delete_remote_tbl_each("REMOTE_PROVIDES", *prepos);
- 				/* update remote* table for this repository */
- 				insert_summary(sumsw[i], summary, *prepos);
- 			}
