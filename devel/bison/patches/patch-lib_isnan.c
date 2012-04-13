c96d35336ebe7b69c057a2d749620e77306d3697
Filip Hajny <filip@joyent.com>
2012-04-13 22:17:28 +0000

    Fix broken patch file

--- devel/bison/patches/patch-lib_isnan.c
+++ devel/bison/patches/patch-lib_isnan.c
@@ -2,10 +2,12 @@ $NetBSD: patch-lib_isnan.c,v 1.1 2012/03/15 07:50:22 jklos Exp $
 
 --- lib/isnan.c.orig	2011-04-16 19:08:41.000000000 +0000
 +++ lib/isnan.c
-@@ -119,6 +119,7 @@ FUNC (DOUBLE x)
+@@ -118,7 +118,8 @@ FUNC (DOUBLE x)
+   /* Be careful to not do any floating-point operation on x, such as x == x,
       because x may be a signaling NaN.  */
  #  if defined __TINYC__ || defined __SUNPRO_C || defined __DECC \
-       || (defined __sgi && !defined __GNUC__) || defined __ICC
+-      || (defined __sgi && !defined __GNUC__) || defined __ICC
++      || (defined __sgi && !defined __GNUC__) || defined __ICC \
 +      || defined __vax__
    /* The Sun C 5.0, Intel ICC 10.0, and Compaq (ex-DEC) 6.4 compilers don't
       recognize the initializers as constant expressions.  The latter compiler
