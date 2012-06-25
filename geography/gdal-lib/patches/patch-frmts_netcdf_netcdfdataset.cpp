e6b82b484ece3b83d44999885913d87cd38ce399
Filip Hajny <filip@joyent.com>
2012-06-25 11:41:30 +0000

    Fix build with GCC 4.7

--- geography/gdal-lib/patches/patch-frmts_netcdf_netcdfdataset.cpp
+++ geography/gdal-lib/patches/patch-frmts_netcdf_netcdfdataset.cpp
@@ -1,3 +1,16 @@
+$NetBSD$
+
+--- frmts/netcdf/netcdfdataset.cpp.orig	2012-01-04 07:03:37.000000000 +0000
++++ frmts/netcdf/netcdfdataset.cpp
+@@ -962,7 +962,7 @@ void  netCDFRasterBand::CheckValidData (
+             /* check for nodata and nan */
+             if ( CPLIsEqual( (double) ((T *)pImage)[i], dfNoDataValue ) )
+                 continue;
+-            if( bCheckIsNan && CPLIsNan( ( (T *) pImage)[i] ) ) {
++            if( bCheckIsNan && CPLIsNan( (double) ( (T *) pImage)[i] ) ) {
+                 ( (T *)pImage )[i] = (T)dfNoDataValue;
+                 continue;
+             }
 $NetBSD: patch-frmts_netcdf_netcdfdataset.cpp,v 1.1 2012/05/04 13:27:23 hans Exp $
 
 --- frmts/netcdf/netcdfdataset.cpp.orig	2012-01-04 08:03:37.000000000 +0100
