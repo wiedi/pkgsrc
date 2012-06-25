34964143b210a89df6fa31062ecff8838fb5c3ef
Filip Hajny <filip@joyent.com>
2012-06-25 11:42:24 +0000

    www/nginx: Add compiled-in Passenger support

--- www/nginx/options.mk
+++ www/nginx/options.mk
@@ -12,6 +12,8 @@ PKG_SUGGESTED_OPTIONS=	inet6 pcre ssl
 
 PLIST_VARS+=		naxsi perl uwsgi
 
+PKG_SUPPORTED_OPTIONS+=	passenger
+
 .include "../../mk/bsd.options.mk"
 
 # documentation says naxsi must be the first module
@@ -202,6 +204,24 @@ CONFIGURE_ARGS+=	--with-http_image_filter_module
 
 .if !empty(PKG_OPTIONS:Mstatus)
 CONFIGURE_ARGS+=	--with-http_stub_status_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mpassenger)
+PKGNAME=		${DISTNAME:S/nginx/nginx-passenger/}
+
+DEPENDS+=		${RUBY_PKGPREFIX}-passenger-[0-9]*:../../wip/ruby-passenger
+
+CONFIGURE_ARGS+=	--add-module=${WRKDIR}/passenger/ext/nginx
+
+MESSAGE_SRC=		MESSAGE
+MESSAGE_SRC+=		MESSAGE.passenger
+
+.include "../../wip/ruby-passenger/inplace.mk"
+
+pre-configure: build-passenger-files
+
+build-passenger-files:
+	cd ${WRKDIR}/passenger/build && ${RAKE} nginx:clean nginx
 .endif
 
 .if !empty(PKG_OPTIONS:Mperl)
