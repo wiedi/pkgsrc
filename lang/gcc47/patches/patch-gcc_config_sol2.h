d062d990b40374f28ee62397292a69d365dc2f95
Jonathan Perkin <jperkin@joyent.com>
2012-08-23 14:55:37 +0100

    Make the GCC multiarch stuff actually work.

--- lang/gcc47/patches/patch-gcc_config_sol2.h
+++ lang/gcc47/patches/patch-gcc_config_sol2.h
@@ -1,15 +1,16 @@
 $NetBSD$
 
---- gcc/config/sol2.h.orig	Mon Aug 20 13:12:58 2012
-+++ gcc/config/sol2.h	Mon Aug 20 13:13:30 2012
-@@ -149,8 +149,8 @@
+--- gcc/config/sol2.h.orig	2011-08-18 14:47:46.000000000 +0000
++++ gcc/config/sol2.h	2012-08-23 09:24:51.940429748 +0000
+@@ -149,8 +149,9 @@
    "%{G:-G} \
     %{YP,*} \
     %{R*} \
 -   %{!YP,*:%{p|pg:-Y P,%R/usr/ccs/lib/libp:%R/usr/lib/libp:%R/usr/ccs/lib:%R/lib:%R/usr/lib} \
 -	   %{!p:%{!pg:-Y P,%R/usr/ccs/lib:%R/lib:%R/usr/lib}}}"
-+   %{!YP,*:%{p|pg:-Y P,%R/usr/ccs/lib/libp:%R/usr/lib/libp:%R/usr/ccs/lib:%R/lib:%R/usr/lib:%R@PREFIX@} \
-+	   %{!p:%{!pg:-Y P,%R/usr/ccs/lib:%R/lib:%R/usr/lib:%R@PREFIX@}}}"
++   -R@PREFIX@/lib \
++   %{!YP,*:%{p|pg:-Y P,%R/usr/ccs/lib/libp:%R/usr/lib/libp:%R/usr/ccs/lib:%R/lib:%R/usr/lib:%R@PREFIX@/lib} \
++	   %{!p:%{!pg:-Y P,%R/usr/ccs/lib:%R/lib:%R/usr/lib:%R@PREFIX@/lib}}}"
  
  #undef LINK_ARCH32_SPEC
  #define LINK_ARCH32_SPEC LINK_ARCH32_SPEC_BASE
