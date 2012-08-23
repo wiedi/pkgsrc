10fbaaf51cd74cb032871c363663ca0ea46050dc
Jonathan Perkin <jperkin@joyent.com>
2012-08-23 14:55:37 +0100

    Make the GCC multiarch stuff actually work.

--- lang/gcc47/patches/patch-gcc_config_sol2-bi.h
+++ lang/gcc47/patches/patch-gcc_config_sol2-bi.h
@@ -1,15 +1,16 @@
 $NetBSD$
 
---- gcc/config/sol2-bi.h.orig	Mon Aug 20 13:10:07 2012
-+++ gcc/config/sol2-bi.h	Mon Aug 20 13:11:54 2012
-@@ -74,8 +74,8 @@
+--- gcc/config/sol2-bi.h.orig	2011-07-07 09:24:16.000000000 +0000
++++ gcc/config/sol2-bi.h	2012-08-23 09:22:45.070296391 +0000
+@@ -74,8 +74,9 @@
    "%{G:-G} \
     %{YP,*} \
     %{R*} \
 -   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR "}	\
 -	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR "}}}"
-+   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/" ARCH64_SUBDIR "}	\
-+	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/" ARCH64_SUBDIR "}}}"
++   -R@PREFIX@/lib/" ARCH64_SUBDIR " \
++   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib/" ARCH64_SUBDIR "}	\
++	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib/" ARCH64_SUBDIR "}}}"
  
  #undef LINK_ARCH64_SPEC
  #ifndef USE_GLD
