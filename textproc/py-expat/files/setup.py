2c25582de4b1d77e899e7fa9f6364001c2ba1e14
Jonathan Perkin <jperkin@joyent.com>
2012-10-12 12:23:14 +0100

    Expand LIBARCHSUFFIX correctly.

--- textproc/py-expat/files/setup.py
+++ textproc/py-expat/files/setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup, Extension
 
 expatprefix = '@EXPATPREFIX@'
 expatincl = expatprefix + '/include'
-expatlib = expatprefix + '/lib${LIBARCHSUFFIX}'
+expatlib = expatprefix + '/lib@LIBARCHSUFFIX@'
 expatrtprefix = '@EXPATPREFIX@'
-expatrtlib = expatrtprefix + '/lib${LIBARCHSUFFIX}'
+expatrtlib = expatrtprefix + '/lib@LIBARCHSUFFIX@'
 
 setup(
 	ext_modules = [
