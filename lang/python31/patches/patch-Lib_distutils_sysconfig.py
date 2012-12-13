476c939a10ccba92e788bbb597461a13ff3bde42
Jonathan Perkin <jperkin@joyent.com>
2012-12-13 12:45:07 +0000

    Convert lang/python31 to multiarch.

---
+++ lang/python31/patches/patch-Lib_distutils_sysconfig.py
@@ -0,0 +1,29 @@
+$NetBSD$
+
+Support multiarch.
+
+--- Lib/distutils/sysconfig.py.orig	2012-04-09 23:25:34.000000000 +0000
++++ Lib/distutils/sysconfig.py	2012-12-13 12:14:50.530986654 +0000
+@@ -122,6 +122,10 @@
+         if standard_lib:
+             return libpython
+         else:
++            if sys.maxsize > 2**32:
++                return os.path.join(libpython, "site-packages", "@LIBARCHSUFFIX.64@".lstrip('/')).rstrip('/')
++            else:
++                return os.path.join(libpython, "site-packages", "@LIBARCHSUFFIX.32@".lstrip('/')).rstrip('/')
+             return os.path.join(libpython, "site-packages")
+     elif os.name == "nt":
+         if standard_lib:
+@@ -226,7 +230,10 @@
+         return os.path.join(os.path.dirname(os.path.realpath(sys.executable)),
+                             "Makefile")
+     lib_dir = get_python_lib(plat_specific=1, standard_lib=1)
+-    return os.path.join(lib_dir, "config", "Makefile")
++    if sys.maxsize > 2**32:
++        return os.path.join(lib_dir, "config", "@LIBARCHSUFFIX.64@".lstrip('/'), "Makefile")
++    else:
++        return os.path.join(lib_dir, "config", "@LIBARCHSUFFIX.32@".lstrip('/'), "Makefile")
+ 
+ 
+ def parse_config_h(fp, g=None):
