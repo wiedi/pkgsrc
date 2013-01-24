760b8e36df5310244401b40a9efa9e12847a9108
Jonathan Perkin <jperkin@joyent.com>
2013-01-24 10:45:12 +0000

    Upgrade security/openssl to 1.0.1c

--- security/openssl/buildlink3.mk
+++ security/openssl/buildlink3.mk
@@ -7,8 +7,8 @@ OPENSSL_BUILDLINK3_MK:=
 
 .  include "../../mk/bsd.fast.prefs.mk"
 
-BUILDLINK_API_DEPENDS.openssl+=	openssl>=0.9.6m
-BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=0.9.8knb1
+BUILDLINK_API_DEPENDS.openssl+=	openssl>=1.0.1c
+BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=1.0.1c
 BUILDLINK_PKGSRCDIR.openssl?=	../../security/openssl
 
 # Ensure that -lcrypt comes before -lcrypto when linking so that the
