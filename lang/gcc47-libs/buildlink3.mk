1c7b7ec06776502af0d1fc2d5b6ee44fbdd17332
Jonathan Perkin <jperkin@joyent.com>
2013-01-24 11:14:26 +0000

    Some fixes to lang/gcc47-libs:
     - Remove the USE_PKGSRC_GCC_RUNTIME local change, it is no longer
       necessary (someone forgot to bump gcc47-libs past gcc47).
    
     - Remove check for GCC47_BUILDLINK_MK, it is incompatible with using
       a gcc from the same pkgsrc environment.
    
     - Put the gcc47-libs path first in the RPATH.

--- lang/gcc47-libs/buildlink3.mk
+++ lang/gcc47-libs/buildlink3.mk
@@ -2,14 +2,10 @@
 
 BUILDLINK_TREE+=	gcc47-libs
 
-.if !defined(GCC47_LIBS_BUILDLINK3_MK) && !defined(GCC47_BUILDLINK3_MK)
+.if !defined(GCC47_LIBS_BUILDLINK3_MK)
 GCC47_LIBS_BUILDLINK3_MK:=
 
-.if !empty(USE_PKGSRC_GCC_RUNTIME:M[Yy][Ee][Ss])
-BUILDLINK_API_DEPENDS.gcc47-libs+=	gcc47-libs>=4.7.0
-.else
 BUILDLINK_API_DEPENDS.gcc47-libs+=	{gcc47,gcc47-libs}>=4.7.0
-.endif
 BUILDLINK_PKGSRCDIR.gcc47-libs=		../../lang/gcc47-libs
 BUILDLINK_DEPMETHOD.gcc47-libs?=	full
 
@@ -30,7 +26,7 @@ GCC_TARGET_MACHINE?= 	${MACHINE_GNU_PLATFORM}
 # XXX: Using %M requires patched version of gcc (e.g. lang/gcc47)
 ${SPECS_LIBGCC}:
 	@${ECHO} "*link_libgcc:" >${SPECS_LIBGCC}
-	@${ECHO} "%D $(LINKER_RPATH_FLAG)${GCC_PREFIX}/lib/%M $(LINKER_RPATH_FLAG)${GCC_PREFIX}/${GCC_TARGET_MACHINE}/lib/%M" >>${SPECS_LIBGCC}
+	@${ECHO} "%D $(LINKER_RPATH_FLAG)${GCC_PREFIX}/${GCC_TARGET_MACHINE}/lib/%M $(LINKER_RPATH_FLAG)${GCC_PREFIX}/lib/%M" >>${SPECS_LIBGCC}
 
 _WRAP_EXTRA_ARGS.CC+=	-specs ${SPECS_LIBGCC}
 _WRAP_EXTRA_ARGS.CXX+=	-specs ${SPECS_LIBGCC}
