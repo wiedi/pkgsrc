1b8ba87a6c234fc168b1f9be1d502a77548722a7
Jonathan Perkin <jperkin@joyent.com>
2013-09-09 15:44:40 +0100

    wip for multiple method scripts.

--- mk/smf.mk
+++ mk/smf.mk
@@ -6,66 +6,89 @@ SMF_MK=				# defined
 
 # Directory to hold the SMF manifest/method files
 PKG_SMF_DIR?=			lib/svc
+PKG_SMF_MANIFEST_DIR?=		${PKG_SMF_DIR}/manifest
+PKG_SMF_METHOD_DIR?=		${PKG_SMF_DIR}/method
 
 # Prefix of SMF services FMRI
 SMF_PREFIX?=			pkgsrc
 
 # Variables that can be overriden by the user on a package by package basis
 SMF_NAME?=			${PKGBASE:tl}
-SMF_INSTANCE?=			default
+SMF_INSTANCES?=			default
 SMF_MANIFEST?=			manifest.xml
-SMF_METHOD?=			method
-
-SMF_MANIFEST_FILE_SRC?=		${FILESDIR}/smf/${SMF_MANIFEST}
-SMF_METHOD_FILE_SRC?=		${FILESDIR}/smf/${SMF_METHOD}
-
-SMF_MANIFEST_FILE?=		${PKG_SMF_DIR}/manifest/${SMF_NAME}.xml
-SMF_METHOD_FILE?=		${PKG_SMF_DIR}/method/${SMF_NAME}
+SMF_METHODS?=			# empty
+SMF_SRCDIR?=			${FILESDIR}/smf
 
 # A manifest file is a pre-requisite for anything to happen
-.  if exists(${SMF_MANIFEST_FILE_SRC})
-SMF_TARGETS+=			${DESTDIR}${PREFIX}/${SMF_MANIFEST_FILE}
+.  if exists(${SMF_SRCDIR}/${SMF_MANIFEST})
 
-INSTALLATION_DIRS+=		${PKG_SMF_DIR}/manifest ${PKG_SMF_DIR}/method
+SMF_MANIFEST_SRC?=		${SMF_SRCDIR}/${SMF_MANIFEST}
+SMF_MANIFEST_WRK?=		${WRKDIR}/.smf_${SMF_MANIFEST}
+SMF_MANIFEST_FILE?=		${PKG_SMF_MANIFEST_DIR}/${SMF_NAME}.xml
+
+FILES_SUBST+=			PKGMANDIR=${PKGMANDIR:Q}
+FILES_SUBST+=			SMF_PREFIX=${SMF_PREFIX:Q}
+FILES_SUBST+=			SMF_NAME=${SMF_NAME:Q}
+FILES_SUBST+=			SMF_INSTANCES=${SMF_INSTANCES:Q}
+FILES_SUBST+=			SMF_MANIFEST=${SMF_MANIFEST:Q}
+FILES_SUBST+=			SMF_MANIFEST_FILE=${SMF_MANIFEST_FILE:Q}
+
+INSTALLATION_DIRS+=		${PKG_SMF_MANIFEST_DIR}
+
+.PHONY: generate-smf-manifest
+generate-smf-manifest: ${SMF_MANIFEST_WRK}
+${SMF_MANIFEST_WRK}: ${SMF_MANIFEST_SRC}
+	@${STEP_MSG} "Creating ${.TARGET}"
+	${RUN}${CAT} ${.ALLSRC} | ${SED} ${FILES_SUBST_SED} > ${.TARGET}
+
+.PHONY: install-smf-manifest
+post-install: install-smf-manifest
+install-smf-manifest: ${SMF_MANIFEST_WRK}
+	${INSTALL_DATA} ${SMF_MANIFEST_WRK} ${DESTDIR}${PREFIX}/${SMF_MANIFEST_FILE}
+
 GENERATE_PLIST+=		${ECHO} "${SMF_MANIFEST_FILE}";
 PRINT_PLIST_AWK+=		/^${SMF_MANIFEST_FILE:S|/|\\/|g}/ { next; }
 
-FILES_SUBST+=			PKGMANDIR=${PKGMANDIR}
-FILES_SUBST+=			SMF_PREFIX=${SMF_PREFIX}
-FILES_SUBST+=			SMF_NAME=${SMF_NAME}
-FILES_SUBST+=			SMF_INSTANCE=${SMF_INSTANCE}
-FILES_SUBST+=			SMF_MANIFEST=${SMF_MANIFEST}
-FILES_SUBST+=			SMF_METHOD=${SMF_METHOD}
-FILES_SUBST+=			SMF_MANIFEST_FILE=${SMF_MANIFEST_FILE}
-FILES_SUBST+=			SMF_METHOD_FILE=${SMF_METHOD_FILE}
-
 # Target to add the INSTALL script to auto-import SMF manifest using svccfg
 ${WRKDIR}/.smfinstall: ${PKGSRCDIR}/mk/install/install-smf
-	${CP} ${PKGSRCDIR}/mk/install/install-smf ${WRKDIR}/.smfinstall
+	@${CP} ${PKGSRCDIR}/mk/install/install-smf ${WRKDIR}/.smfinstall
 
 INSTALL_TEMPLATES+=		${WRKDIR}/.smfinstall
 
-# Target to install the SMF manifest
-${DESTDIR}${PREFIX}/${SMF_MANIFEST_FILE}: ${SMF_MANIFEST_FILE_SRC}
-	@${SED} ${FILES_SUBST_SED} ${SMF_MANIFEST_FILE_SRC} > ${WRKDIR}/.smf_${SMF_MANIFEST}
-	${INSTALL_DATA} ${WRKDIR}/.smf_${SMF_MANIFEST} ${DESTDIR}${PREFIX}/${SMF_MANIFEST_FILE}
+# Install optional SMF methods
+#
+.PHONY: generate-smf-methods
+generate-smf-methods:	# do nothing
 
-# A method script file is not required
-.    if exists(${SMF_METHOD_FILE_SRC})
-SMF_TARGETS+=			${DESTDIR}${PREFIX}/${SMF_METHOD_FILE}
-GENERATE_PLIST+=		${ECHO} "${SMF_METHOD_FILE}";
-PRINT_PLIST_AWK+=		/^${SMF_METHOD_FILE:S|/|\\/|g}/ { next; }
+.PHONY: install-smf-methods
+post-install: install-smf-methods
+install-smf-methods:	# do nothing
 
-# Target to install the SMF method script
-${DESTDIR}${PREFIX}/${SMF_METHOD_FILE}:
-	@${SED} ${FILES_SUBST_SED} ${SMF_METHOD_FILE_SRC} > ${WRKDIR}/.smf_${SMF_METHOD}
-	${INSTALL_SCRIPT} ${WRKDIR}/.smf_${SMF_METHOD} ${DESTDIR}${PREFIX}/${SMF_METHOD_FILE}
-.    endif
+.    for _method_ in ${SMF_METHODS}
+SMF_METHOD_SRC.${_method_}?=	${SMF_SRCDIR}/${_method_}.sh
+SMF_METHOD_WRK.${_method_}?=	${WRKDIR}/.smf_${_method_}
+SMF_METHOD_FILE.${_method_}?=	${PKG_SMF_METHOD_DIR}/${_method_}
 
-.PHONY: smf-message
-smf-message:
-	@${STEP_MSG} "Generating SMF files for ${PKGNAME}"
+FILES_SUBST+=	SMF_METHOD_FILE.${_method_}=${SMF_METHOD_FILE.${_method_}}
 
-post-install: smf-message ${SMF_TARGETS}
+.      if !empty(SMF_METHOD_SRC.${_method_})
+generate-smf-methods: ${SMF_METHOD_WRK.${_method_}}
+${SMF_METHOD_WRK.${_method_}}: ${SMF_METHOD_SRC.${_method_}}
+	@${STEP_MSG} "Creating ${.TARGET}"
+	${RUN}${CAT} ${.ALLSRC} | ${SED} ${FILES_SUBST_SED} > ${.TARGET}
+	${RUN}${CHMOD} +x ${.TARGET}
+
+install-smf-methods: install-smf-${_method_}
+install-smf-${_method_}: ${SMF_METHOD_WRK.${_method_}}
+	${RUN} \
+	if [ -f ${SMF_METHOD_WRK.${_method_}} ]; then \
+		${MKDIR} ${DESTDIR}${PREFIX}/${PKG_SMF_METHOD_DIR}; \
+		${INSTALL_SCRIPT} ${SMF_METHOD_WRK.${_method_}} \
+			${DESTDIR}${PREFIX}/${SMF_METHOD_FILE.${_method_}}; \
+	fi
+.      endif
+GENERATE_PLIST+=	${ECHO} ${SMF_METHOD_FILE.${_method_}};
+PRINT_PLIST_AWK+=	/^${SMF_METHOD_FILE.${_method_}:S|/|\\/|g}/ { next; }
+.    endfor
 .  endif
 .endif
