c22305203007829b69f70ac0ae82422088a1cbd8
Sebastian Wiedenroth <wiedi@frubar.net>
2013-10-24 01:20:30 +0200

    ne: fix build on SmartOS

---
+++ editors/ne/patches/patch-sysunix.c
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- sysunix.c.orig	2003-10-01 09:57:29.000000000 +0000
++++ sysunix.c
+@@ -21,6 +21,10 @@ its own modules. */
+ #include <fcntl.h>
+ #include <dirent.h>
+             
++#if defined(__sun)
++# include <sys/filio.h>
++#endif
++
+ #include "ehdr.h"
+ #include "unixhdr.h"
+ #include "scomhdr.h"
