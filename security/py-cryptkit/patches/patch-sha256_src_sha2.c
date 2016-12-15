0c5237f384037e3ed3ece5d3742c7dd306fb2f31
Sebastian Wiedenroth <wiedi@frubar.net>
2016-12-15 01:03:41 +0000

    fix up integer types and BYTE_ORDER on SunOS

---
+++ security/py-cryptkit/patches/patch-sha256_src_sha2.c
@@ -0,0 +1,16 @@
+$NetBSD$
+
+need endian.h on SunOS for BYTE_ORDER
+
+--- sha256/src/sha2.c.orig	2001-11-29 15:56:49.000000000 +0000
++++ sha256/src/sha2.c
+@@ -36,6 +36,9 @@
+  *
+  */
+ 
++#if defined(__sun)
++#include <endian.h>
++#endif
+ 
+ #include <string.h>	/* memcpy()/memset() or bcopy()/bzero() */
+ #include <assert.h>	/* assert() */
